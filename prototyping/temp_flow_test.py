7bfc5980f2fe6286ac102de8f77e65e0017e9799
Brendan Shanny <brendan.shanny@gmail.com>
2018-10-08 19:58:48 -0700

    Add in sys import and one second sleep

--- prototyping/temp_flow_test.py
+++ prototyping/temp_flow_test.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import time
+import sys
 
 from water_flow import WaterFlowMonitor
 from temperature_probe import TemperatureMonitor
@@ -11,15 +12,14 @@ TempMonitor = TemperatureMonitor()
 
 print("Running temperature probe and water flow monitor")
 timer = time.time() + 10
-minutes = 0
 while True:
     try:
         if time.time() >= timer:
-            minutes += 1
             FlowMonitor.calc_flow()
             FlowMonitor.reset_rate_count()
             timer = time.time() + 10
             print(TempMonitor.read_temp())
+        time.sleep(1)
     except KeyboardInterrupt:
         FlowMonitor.shut_down()
         sys.exit()
