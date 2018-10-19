9b63f4a510d9b4852fd30b9efaf1b9b7f57eec98
Brendan Shanny <brendan.shanny@gmail.com>
2018-10-18 22:53:39 -0700

    Pass 3

--- prototyping/water_level.py
+++ prototyping/water_level.py
@@ -8,9 +8,10 @@ ldr_channel = 0
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
+spi.max_speed_hz = 1350000
 
 def read(adcnum):
-    if adcnum > 7 || adcnum < 0:
+    if adcnum > 7 or adcnum < 0:
         return -1
     r = spi.xfer2([1, 8 + adcnum << 4, 0])
     data = ((r[1] & 3) << 8) + r[2]
@@ -18,6 +19,7 @@ def read(adcnum):
 
 while True:
     ldr_value = read(ldr_channel)
+    ohms = round(((ldr_value * 3300) / 1024),0)
     print("----------------------------")
     print("Water Level: {}".format(ldr_value))
     time.sleep(delay)
